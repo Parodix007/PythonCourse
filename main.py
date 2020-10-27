31bae5b193547544d58c4cb8830af6851b1ce454
Parodix007 <sebastian.siarczynski@gmail.com>
2020-10-27 15:49:16 +0100

    Cwiczenia na strukturach danych

--- main.py
+++ main.py
@@ -43,21 +43,43 @@ elif fb % 5 == 0:
 elif fb % 5 == 0 and fb % 3 == 0:
     print('FizzBuzz')
 
+os.system('clear')
 
+for item in range(10):
+    if item % 3 == 0:
+        print('Fizz')
+    elif item % 5 == 0:
+        print('Buzz')
+    elif item % 5 == 0 and fb % 3 == 0:
+        print('FizzBuzz')
 
+os.system('clear')
+
+b = 0
+while b < 10:
+    if b % 3 == 0:
+        print('Fizz')
+    elif b % 5 == 0:
+        print('Buzz')
+    elif b % 5 == 0 and fb % 3 == 0:
+        print('FizzBuzz')
+    b +=1
+ 
 
 
 #? I/O w Pythonie
 
 #TODO: funkcja input() przyjmuje argument opcjonalny ktorym jest zazwyczaj string, jest on wyswietlany jako wiadomosc do uzytkownika ktory bedzie wprowadzal wartosc do programu.
-
+os.system('clear')
 
 #input_ = input('Podaj wartosc zmiennej: ')
+
 
 #TODO: funkcja print() dziala identycznie lecz przyjmuje drugi argument ktorym jest zmienna ktora bedzie wyswietlana w terminalu.
 
 #print("wartosc:",input_)
 
+os.system('clear')
 
 #? Data Types
 
@@ -66,16 +88,57 @@ elif fb % 5 == 0 and fb % 3 == 0:
 
 #TODO: list jest w zasadzie typem Tablicy zmiennym dynamicznie
 
-#list_ = [1,2,3,4]
+list_ = [1, 2, 3, 4, True, 'sadas']
+
+print(list_[0])
 # index []
 # len()
 # append
+list_.append('costam')
+print(list_)
 # insert
+list_.insert(1, 1234)
+print(list_)
 # remove
+list_.remove(2)
+print(list_)
+
+
+
+os.system('clear')
+
+
+
+# fiblist = [1,1]
+# n = 20
+
+# for item in range(n):
+#     fiblist.append(fiblist[-1] + fiblist[-2])
+
+# print(fiblist)
+
+
+lista1 = [10,20,30,20,60,100,120,20,30,80,60]
+lista2 = []
+
+for item in lista1:
+    if item not in lista2:
+        lista2.append(item)
+
+print(lista2)
+
 
 #TODO: jest to typem Tablicy o wartosciach niezmiennych
 
 #tuple_ = (1,2,3,4)
+
+
+
+
+
+
+
+
 
 #TODO: Jest to struktura danych nazywana takze w czasami tablica asocjacyjna
 
